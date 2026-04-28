4adff8db95ad05a37142083bdecdb382596b5fab
Rushil Umaretiya <rushilwiz@gmail.com>
2026-04-28 08:15:09 -0400

    feat: updated db pool for socket connection

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -1,10 +1,19 @@
 import mysql from "mysql2/promise";
+
+const isProduction = process.env.NODE_ENV === "production";
+
 const pool = await mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+  ...(isProduction
+    ? { socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}` }
+    : {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+      }),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 5,
 });
 
 export default pool;
